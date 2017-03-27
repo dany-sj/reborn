3a16deac3fe267758f24eb024f87cfad938b4d18
EVILBOT <danyal.jafari2014@gmail.com>
2017-03-28 01:16:47 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,5 +1,5 @@
 -- #Beyond Reborn Robot
--- #@BeyondTeam
+-- #@Plus_Mod
 
 tdcli = dofile('./tg/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
@@ -93,43 +93,18 @@ function create_config( )
 		"msg-checks",
 		"plugins",
 		"tools",
-		"muteall"
+		"muteall",
+		"info",
 	},
     sudo_users = {
-   157059515,
+   223445853,
    0
 },
     admins = {},
 	cmd = '^[/!#]',
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v4.0
-An advanced administration bot based on https://valtman.name/telegram-cli
-
-》https://github.com/BeyondTeam/BDReborn 
-
-》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@Rixel ➣ Developer 《
-》@Exacute ➣ Developer《
-》@To0fan ➣ Developer《
-》@CiveY ➣ Developer
-》@K_a_I_i_I_i_n_u_x ➣ Developer《
-》@ArmanDev ➣ Manager《
-》@MrPars ➣ Manager《
-
-》Special thanks to :
-》@Vysheng
-》@MrHalix
-》@Nero_Dev
-》And Beyond Team Members
-
-》Our channel :
-》@BeyondTeam《
-
-》Our website :
-》http://BeyondTeam.ir
+    info_text = [[》
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
